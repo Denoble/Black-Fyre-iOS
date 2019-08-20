f774531acbc53f47d8ff3d78aa4a528c7815fead
uchegodfrey <ujay2002@yahoo.com>
2019-08-20 18:30:32 -0400

    Black Fyre ios first

---
+++ BlackFyre/BlackFyre/LaunchScreen.swift
@@ -0,0 +1,9 @@
+//
+//  LaunchScreen.swift
+//  BlackFyre
+//
+//  Created by Uche Godfrey on 2019-08-19.
+//  Copyright © 2019 BlackFyre. All rights reserved.
+//
+
+import Foundation
---
+++ BlackFyre/BlackFyre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackFyre
+//
+//  Created by Uche Godfrey on 2019-08-18.
+//  Copyright © 2019 BlackFyre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
